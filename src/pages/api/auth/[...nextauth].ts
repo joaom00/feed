3fe3eb5aa96b4f5f1e8a7f4585f4928b14b7f1c5
João Pedro Magalhães <joaopsilvamagalhaes@gmail.com>
2022-09-05 14:48:17 -0300

    chore: format stuff

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -21,9 +21,9 @@ export const authOptions: NextAuthOptions = {
       clientSecret: process.env.GITHUB_CLIENT_SECRET
     }),
     GoogleProvider({
-clientId: process.env.GOOGLE_CLIENT_ID,
+      clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET
-    }),
+    })
   ],
   callbacks: {
     async session({ session, user: userSession }) {
